1a88c415aa916c1357115dd48d2fda721486d169
user <user@MacBook-Pro-user.local>
2022-10-24 20:16:34 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        name("adil","aitbaev");
+
+    }public static String[] name(String ... names){
+
+        for (String aa: names ) {
+            System.out.println("love you "+ aa);
+        }
+        return names;
+
+    }
+}
